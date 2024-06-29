89a723477db0da15ef22861b25c8988eb558ec55
alex garag <alex.garag@bk.ru>
2024-06-29 18:05:24 +0300

    вроде, решено

---
+++ src/by/tms/lessonc30onl/throwinshapes/container/Box.java
@@ -0,0 +1,15 @@
+package by.tms.lessonc30onl.throwinshapes.body;
+
+import by.tms.lessonc30onl.throwinshapes.baseclass.Shape;
+
+public class Box extends Shape {
+
+    public Box(double settingVolume) {
+        super(settingVolume);
+    }
+
+//    @Override
+//    public double getVolume() {
+//        return boxVolume;
+//    }
+}
