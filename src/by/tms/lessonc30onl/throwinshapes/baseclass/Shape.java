4916f6227cd617518392014388d931529707bcef
alex garag <alex.garag@bk.ru>
2024-06-29 16:38:19 +0300

    восстанавливаю (на новой ветке?)

--- src/by/tms/lessonc30onl/throwinshapes/baseclass/Shape.java
+++ src/by/tms/lessonc30onl/throwinshapes/baseclass/Shape.java
@@ -1,9 +1,7 @@
 package by.tms.lessonc30onl.throwinshapes;
 
-public abstract class Shape implements CapableGivingVolume {
+public class Shape implements CapableGivingVolume {
     private double volume;
-
-//    public Shape(double volume) {}
 
     public double getVolume() {
         return volume;
