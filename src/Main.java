8412f5493e8fb13aa3e5b5b2fd071d7f22245203
alex garag <alex.garag@bk.ru>
2024-06-30 12:14:18 +0300

    перенес генерацию форм в класс Форма

--- src/Main.java
+++ src/Main.java
@@ -1,12 +1,12 @@
+import by.tms.lessonc30onl.throwinshapes.baseclass.Shape;
 import by.tms.lessonc30onl.throwinshapes.boxclass.Box;
-import by.tms.lessonc30onl.throwinshapes.baseclass.ShapeGenerator;
 
 public class Main {
     public static void main(String[] args) {
         Box box = new Box((int) (255 * Math.random())); // assigning the volume of the box randomly (maximum 255)
         int quantityShape = 0;
         do {
-            if (box.add(ShapeGenerator.generateShape())) {
+            if (box.add(Shape.generateShape())) {
                 quantityShape = quantityShape + 1;
             } else {
                 break;
--- src/by/tms/lessonc30onl/throwinshapes/baseclass/Shape.java
+++ src/by/tms/lessonc30onl/throwinshapes/baseclass/Shape.java
@@ -1,5 +1,6 @@
 package by.tms.lessonc30onl.throwinshapes.baseclass;
 
+import by.tms.lessonc30onl.throwinshapes.bodyClass.*;
 import by.tms.lessonc30onl.throwinshapes.interfaces.CapableGivingVolume;
 
 public class Shape implements CapableGivingVolume {
@@ -14,5 +15,23 @@ public class Shape implements CapableGivingVolume {
 
     public double getVolume() {
         return volumeShape;
+    }
+
+    public static Shape generateShape() {
+        int randomNumberShape = (int) (5 * Math.random());  // choosing the shape type by generating a random number (one of five)
+        double x = Math.random() * 3;   // assigning shape sizes randomly (from zero to three)
+        double y = Math.random() * 3;
+        double z = Math.random() * 3;
+        switch (randomNumberShape) {
+            case 1:
+                return new Cylinder(x, y);
+            case 2:
+                return new Cone(x, y);
+            case 3:
+                return new Ball(x);
+            case 4:
+                return new Parallelepiped(x, y, z);
+        }
+        return new Pyramid(x, y, z);
     }
 }
